33d683b084b6186eec4e59065bd2da946c5692b4
Alex Clarke <ac3716@ic.ac.uk>
2023-07-25 23:29:21 +0100

    improve hiss drag

--- hiss_drag.py
+++ hiss_drag.py
@@ -7,8 +7,10 @@ from time import sleep, time
 
 start = 0
 running = False
-noise_length_threshold = "100ms"
+noise_length_threshold = "150ms"
 threshold_passed = False
+hiss_release_threshold = "500ms"
+hiss_release_threshold_passed = False
 
 mod = Module()
 setting_mouse_enable_hiss_drag = mod.setting(
@@ -18,6 +20,8 @@ setting_mouse_enable_hiss_drag = mod.setting(
     desc="Enable hiss to drag when control mouse is enabled.",
 )
 
+mod.mode("speech_noise", "noises that can be accidently triggered through speech")
+
 def still_running():
     global running
     global threshold_passed
@@ -26,33 +30,40 @@ def still_running():
         toggle_mouse_drag(True)
         print('hiss duration passed threshold, starting gaze drag')
 
-
+def still_running_hiss_release_threshold():
+    global running
+    global hiss_release_threshold_passed
+    if running:
+        hiss_release_threshold_passed = True
+    
 
 def cursor_drag_on_hiss(is_active):
   if actions.app.name() == "dota2.exe":
     if is_active:
       ctrl.mouse_click(button = 4, hold = 30)
   else:
-    if "noise" in scope.get("mode"):
+    if "noise" in scope.get("mode") and "user.speech_noise" in scope.get("mode"):
+      global start
+      global running
+      global threshold_passed
       if is_active:
-        global start
-        global running
-        global threshold_passed
-        if is_active:
-            print("Start Hiss")
-            if 0 in ctrl.mouse_buttons_down():
-              ctrl.mouse_click(button=0, up=True)
-            else:
-              # ctrl.mouse_click(button=0, down=True)
+          print("Start Hiss")
+          if 0 in ctrl.mouse_buttons_down():
+            ctrl.mouse_click(button=0, up=True)
+          else:
+            # ctrl.mouse_click(button=0, down=True)
+            if not running:
               start = time()
               running = True
               cron.after(noise_length_threshold, still_running)
-        else:
-            running = False
-            if threshold_passed:
-                threshold_passed = False
-                toggle_mouse_drag(False)
-                print('end of hiss detected, disabling gaze drag')
+              cron.after(hiss_release_threshold_passed, still_running_hiss_release_threshold)
+      else:
+          running = False
+          if hiss_release_threshold_passed:
+              threshold_passed = False
+              hiss_release_threshold_passed = False
+              toggle_mouse_drag(False)
+              print('end of hiss detected, disabling gaze drag')
 
 noise.register('hiss', cursor_drag_on_hiss)
 
