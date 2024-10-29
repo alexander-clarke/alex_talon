a3b5455b1bb88e67e64a18abc49b7b10c2da539f
Alex Clarke <alexander-c@outlook.com>
2024-10-29 14:31:51 +0000

    Expand support for unreal macros

--- apps/unreal/cpp/unreal_cpp.py
+++ apps/unreal/cpp/unreal_cpp.py
@@ -1,7 +1,28 @@
-from talon import Module, actions
+from talon import Module, actions, Context
 
 mod = Module()
+
+ctx = Context()
+ctx.matches = r"""
+code.language: c
+code.language: cpp
+"""
+
+mod.list("ue_types", desc="ue types")
+ctx.lists["self.ue_types"] = {
+    "actor": "AActor",
+    "subclass": "TSubclassOf<",
+    "soft object pointer": "FSoftObjectPtr<",
+    "array": "TArray<",
+    "transform": "FTransform",
+    "vector": "FVector",
+    "name": "FName",
+}
+
+mod.list("unreal_log_verbosity", desc="Unreal log verbosity")
+
 mod.list("unreal_ufunction_specifiers", desc="Unreal function specifiers")
+mod.list("unreal_uproperty_specifiers", desc="Unreal uproperty specifiers")
 
 
 @mod.capture(rule="{user.unreal_ufunction_specifiers}+")
@@ -10,13 +31,26 @@ def ufunction_specifiers(m) -> str:
     return ", ".join(m.unreal_ufunction_specifiers_list)
 
 
-mod.list("unreal_uproperty_specifiers", desc="Unreal uproperty specifiers")
+@mod.capture(rule="Cat <user.text>")
+def uspecifiers_category(m) -> str:
+    """A category to be added to a specifier list"""
+    return f'Category="{actions.user.formatted_text(m.text, "CAPITALIZE_ALL_WORDS")}"'
 
 
-@mod.capture(rule="{user.unreal_uproperty_specifiers}+")
+@mod.capture(rule="(<user.uspecifiers_category> | {user.unreal_uproperty_specifiers})")
+def uproperty_specifier(m) -> str:
+    return m.uspecifiers_category or m.unreal_uproperty_specifiers
+
+
+@mod.capture(rule="<user.uproperty_specifier>+")
 def uproperty_specifiers(m) -> str:
     """A sequence of unreal engine uproperty specifiers"""
-    return ", ".join(m.unreal_uproperty_specifiers_list)
+    return ", ".join(m.uproperty_specifier_list)
+
+
+mod.list("unreal_delegate_dynamic", "unreal delegate dynamic")
+mod.list("unreal_delegate_multicast", "unreal delegate multicast")
+mod.list("unreal_delegate_param_count", "unreal delegate parameter count")
 
 
 @mod.action_class
--- apps/unreal/cpp/unreal_cpp.talon
+++ apps/unreal/cpp/unreal_cpp.talon
@@ -5,20 +5,30 @@ code.language: cpp
 state you property:
   insert("UPROPERTY(BlueprintReadWrite, EditAnywhere)")
 
-state you property <user.uproperty_specifiers>:
+state you (property|prop) <user.uproperty_specifiers>:
   user.insert_between("UPROPERTY({uproperty_specifiers}", ")")
 
-insert you property <user.uproperty_specifiers>:
+insert [you] (property|prop) <user.uproperty_specifiers>:
   user.smart_insert_specifiers(uproperty_specifiers)
 
 state read only you property:
   insert("UPROPERTY(BlueprintReadOnly, VisibleAnywhere)")
 
-state you function <user.ufunction_specifiers>:
-  user.insert_between("UFUNCTION({ufunction_specifiers}", ")")
+state you function [<user.ufunction_specifiers>]:
+  user.insert_between("UFUNCTION({ufunction_specifiers or ''}", ")")
   
-insert you function [specifier] <user.ufunction_specifiers>:
+insert [you] function [specifier] <user.ufunction_specifiers>:
   user.smart_insert_specifiers(ufunction_specifiers)
 
-state log:
-  user.insert_between("UE_LOG(LogTemp, ", ")")
+state log [{user.unreal_log_verbosity}]:
+  user.insert_between("UE_LOG(LogTemp, {unreal_log_verbosity or 'Display'}, TEXT('", "'))")
+
+declare [{user.unreal_delegate_dynamic}] [{user.unreal_delegate_multicast}] [{user.unreal_delegate_param_count} [(parameters|param)]] delegate: 
+  insert("DECLARE")
+  insert(unreal_delegate_dynamic or '')
+  insert(unreal_delegate_multicast or '')
+  insert("_DELEGATE")
+  insert(unreal_delegate_param_count or '')
+  user.insert_between("(F", ")")
+
+{user.ue_types}: "{ue_types}" 
